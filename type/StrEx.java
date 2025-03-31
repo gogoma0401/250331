1135df3c0293f5da26b100c14ac95d15ca9b2f20
변준현 <gogoma0401@naver.com>
2025-03-31 16:45:09 +0900

    250331 수업내용

---
+++ type/StrEx.java
@@ -0,0 +1,9 @@
+package type;
+
+public class StrEx {
+	public static void main(String[] args) {
+		String name = "김철수";
+		System.out.println("이름:" + name);
+		System.out.println("나이:" + 20);
+	}
+}
