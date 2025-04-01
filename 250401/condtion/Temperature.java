b2b2c93c815a1f6c84428d49dec883bbf71151e0
변준현 <gogoma0401@naver.com>
2025-04-01 16:36:11 +0900

    2일차

---
+++ 250401/condtion/Temperature.java
@@ -0,0 +1,9 @@
+package condtion;
+import java.util.Scanner;
+public class Temperature {
+	public static void main (String [] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("섭씨 온도:");
+		
+	}
+}
