4d56e10b0ae63af5a2e445871fd3a60439173c0b
변준현 <gogoma0401@naver.com>
2025-04-09 17:56:26 +0900

    8일차

---
+++ 250409/oop/Person2.java
@@ -0,0 +1,34 @@
+package oop;
+
+public class Person2 {
+	public String name;
+	public int age;
+	public double height;
+	
+	public void setHeight(double height) {
+		this.height = height;
+	}
+	
+	public void getHeight() {
+		return height;
+	}
+	public void setAge(int age) {
+		if (age < 0 || age > 150) {
+			System.out.println("error!");
+		} else {
+			this.age = age;
+		}
+	}
+	public String getAge() {
+		return age;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getName() {
+		return name;
+	}
+	public void print() {
+		System.out.println("Name : " + name + ",Age:" + age + ",Height:" + height); 
+	}
+}
